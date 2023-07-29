9eee8630c0ccb0f42b857ccdfdd1814b9d70679b
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-07-29 17:29:42 +0600

    Problem one

---
+++ problem_one.js
@@ -0,0 +1,7 @@
+var inPocket = 1000 ;
+var applePrice = 400 ;
+var orangePrice = 300 ;
+var totalCost = (applePrice + orangePrice) ;
+// console.log(totalCost);
+var nowInPocket = (inPocket - totalCost);
+console.log(nowInPocket) ;
---
+++ variable.js
@@ -0,0 +1,6 @@
+var age = 25;
+var temparature = 38;
+var money = 27000;
+var weight = 46;
+console.log(8888);
+console.log(258);
